d9aecd9e7cd93e763d5ea385af838523d37fae71
NDook <thomasmludek@gmail.com>
2022-05-06 14:38:25 +0200

    mouse-cursor optimizes Yessssgit add .git add .!

--- scripts.js
+++ scripts.js
@@ -1,53 +1,27 @@
+/*mouse-cursor effect*/
 console.log("connecté");
 const mouseCursor = document.querySelector(".mouse-cursor");
-console.log(mouseCursor);
 
 document.body.addEventListener("mousemove", function(event){
-    const mouseCursorTop = event.clientY;
-    const mouseCursorLeft = event.clientX;
-    mouseCursor.style.top = mouseCursorTop + "px";
-    mouseCursor.style.left = mouseCursorLeft + "px";   
-})
+    mouseCursor.style.top = (event.layerY) + "px";
+    mouseCursor.style.left = (event.layerX) + "px";   
+});
 
+/* effet interval "Blur" sur Tilte Page 1 (ko)
+const titletext = document.querySelector(".texttiltle");
 
-
-
-
-
-
-/*function getMouseCoords(e) {
-  var e = e || window.event;
-  document.getElementById('msg').innerHTML = e.clientX + ', ' + 
-           e.clientY + '<br>' + e.screenX + ', ' + e.screenY;
+function titleEffect(titletext) {    
+    if(titletext.style.filter == "none"){
+        titletext.style.filter = "blur(1px)";
+}   else {
+        titletext.style.filter = "none";
+}    
 };
+setInterval(titleEffect, 2000);*/ 
 
-
-var followCursor = (function() {
-  var s = document.createElement('div');
-  s.style.position = 'absolute';
-  s.style.width = "25px";
-  s.style.height = "25px";
-  s.style.margin = '0';
-  s.style.padding = '5px';
-  s.style.background = 'white';
-  s.style.opacity = "0.1";
-  s.style.borderRadius = "80%";
-
-  return {
-    init: function() {
-      document.body.appendChild(s);
-    },
-
-    run: function(e) {
-      var e = e || window.event;
-      s.style.left  = (e.clientX - 15) + 'px';
-      s.style.top = (e.clientY - 15) + 'px';
-      getMouseCoords(e);
-    }
-  };
-}());
-
-window.onload = function() {
-  followCursor.init();
-  document.body.onmousemove = followCursor.run;
-};*/
+/*newletter inscription*/
+document.addEventListener("submit", function(event){ 
+    event.preventDefault(); /*problème event defaut*/ 
+    const newsletter = document.querySelector("#inscription");    
+    newsletter.value = "Inscription envoyée !";   
+});
