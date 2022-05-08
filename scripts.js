226966212f5febe84e443edd5675945d1b8051c9
NDook <thomasmludek@gmail.com>
2022-05-08 18:48:17 +0200

    ok, it's so difficult !

--- scripts.js
+++ scripts.js
@@ -1,27 +1,37 @@
 /*mouse-cursor effect*/
 console.log("connecté");
-const mouseCursor = document.querySelector(".mouse-cursor");
+
 
 document.body.addEventListener("mousemove", function(event){
+    const mouseCursor = document.querySelector(".mouse-cursor");
+    mouseCursor.style.display = "block";
     mouseCursor.style.top = (event.layerY) + "px";
     mouseCursor.style.left = (event.layerX) + "px";   
 });
-
-/* effet interval "Blur" sur Tilte Page 1 (ko)
-const titletext = document.querySelector(".texttiltle");
-
-function titleEffect(titletext) {    
-    if(titletext.style.filter == "none"){
-        titletext.style.filter = "blur(1px)";
-}   else {
-        titletext.style.filter = "none";
-}    
-};
-setInterval(titleEffect, 2000);*/ 
-
 /*newletter inscription*/
 document.addEventListener("submit", function(event){ 
-    event.preventDefault(); /*problème event defaut*/ 
+    event.preventDefault();  
     const newsletter = document.querySelector("#inscription");    
-    newsletter.value = "Inscription envoyée !";   
+    newsletter.value = "Merci ! tu es inscrit.";   
 });
+
+
+
+
+
+
+
+
+
+
+/* effet interval "Blur" sur Tilte Page 1 (ko)
+const titletext = document.querySelector("#texttiltle");
+console.log(titletext.style);
+function titleEffect(titletext) {    
+    if(titletext.style.filter == "blur(0)"){
+        titletext.style.filter = "blur(1px)";
+}   else {
+        titletext.style.filter = "blur(0)";
+}    
+};
+setInterval(titleEffect, 2000);  */ 
